8feb6223e6d5dda962193a4337acc7dcf441f2d3
183tm <17thaicolony@gmail.com>
2024-01-16 10:18:59 +0300

    Задание 16
    Исправления. Убрал одиночные сценарии и убрал фильтр для адреса

--- addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
@@ -35,79 +35,13 @@ public class ContactInfoTests extends TestBase {
 
         expected = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
                 Stream.of(contact.address())
-                        .filter(s -> s != null && !"".equals(s))
+                        //.filter(s -> s != null && !"".equals(s))
                         .collect(Collectors.joining("\n"))
         ));
         var address = app.contacts().getAddress();
         Assertions.assertEquals(expected, address);
 
         expected = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
-                Stream.of(contact.email(), contact.email2(), contact.email3())
-                        .filter(s -> s != null && !"".equals(s))
-                        .collect(Collectors.joining("\n"))
-        ));
-        var emails = app.contacts().getEmails();
-        Assertions.assertEquals(expected, emails);
-    }
-
-
-    @Test
-    void testPhones() {
-        if (app.hbm().getContactCount() == 0) {
-            app.hbm().createContact(new ContactData()
-                    .withFirstName(CommonFunctions.randomString(10))
-                    .withMiddleName(CommonFunctions.randomString(20))
-                    .withLastName(CommonFunctions.randomString(30))
-                    .withNicknameName(CommonFunctions.randomString(40))
-                    .withHome("11111")
-                    .withWork("22222")
-                    .withMobile("33333"));
-        }
-        var contacts = app.hbm().getContactList();
-        var expected = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
-                Stream.of(contact.home(), contact.mobile(),
-                                contact.work(), contact.secondary())
-                        .filter(s -> s != null && !"".equals(s))
-                        .collect(Collectors.joining("\n"))
-        ));
-        var phones = app.contacts().getPhones();
-        Assertions.assertEquals(expected, phones);
-    }
-
-    @Test
-    void testAddress() {
-        if (app.hbm().getContactCount() == 0) {
-            app.hbm().createContact(new ContactData()
-                    .withFirstName(CommonFunctions.randomString(10))
-                    .withMiddleName(CommonFunctions.randomString(20))
-                    .withLastName(CommonFunctions.randomString(30))
-                    .withNicknameName(CommonFunctions.randomString(40))
-                    .withAddress("Kamchatskaya"));
-        }
-        var contacts = app.hbm().getContactList();
-        var expected = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
-                Stream.of(contact.address())
-                        .filter(s -> s != null && !"".equals(s))
-                        .collect(Collectors.joining("\n"))
-        ));
-        var address = app.contacts().getAddress();
-        Assertions.assertEquals(expected, address);
-    }
-
-    @Test
-    void testEmails() {
-        if (app.hbm().getContactCount() == 0) {
-            app.hbm().createContact(new ContactData()
-                    .withFirstName(CommonFunctions.randomString(10))
-                    .withMiddleName(CommonFunctions.randomString(20))
-                    .withLastName(CommonFunctions.randomString(30))
-                    .withNicknameName(CommonFunctions.randomString(40))
-                    .withEmail("testovaya1")
-                    .withEmail2("testovaya2")
-                    .withEmail3("testovaya3"));
-        }
-        var contacts = app.hbm().getContactList();
-        var expected = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
                 Stream.of(contact.email(), contact.email2(), contact.email3())
                         .filter(s -> s != null && !"".equals(s))
                         .collect(Collectors.joining("\n"))
